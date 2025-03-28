12182b40236f4f3a1c4d2bcd8563583aaef9e68f
bastien <bpetit@degetel.com>
2025-03-28 07:55:25 +0100

    blog index implementation filter navigation translations

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/front/app.js')
+    .addEntry('blogIndex', './assets/front/pages/blog/blogIndex.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
